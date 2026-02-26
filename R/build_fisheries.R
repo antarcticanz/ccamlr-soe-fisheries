60d182a7c2a9b641bb17b850db817aa0d29e6075
mikemyoung <mikemeredyth@gmail.com>
2026-02-26 14:55:14 +1300

    Update report output paths and plot code

---
+++ R/build_fisheries.R
@@ -0,0 +1,70 @@
+library(tidyverse)
+library(readr)
+library(purrr)
+library(highcharter)
+options(scipen=999)
+
+# https://www.ccamlr.org/en/fisheries/toothfish-fisheries
+
+if (!dir.exists("ccamlr_data")) {
+dir.create("ccamlr_data")
+}
+
+
+zip_url  <- "https://www.ccamlr.org/en/system/files/CCAMLR_statistical%20bulletin_V37.zip"
+zip_file <- file.path("ccamlr_data", "ccamlr_statistical_bulletin_V37.zip")
+
+download.file(zip_url, destfile = zip_file, mode = "wb")
+
+unzip(zip_file, exdir = "ccamlr_data")
+
+################################################################################
+    ############################## BUILD ############################## 
+################################################################################
+
+
+
+
+ds <- read_csv(
+        "data/ccamlr/ccamlr_statistical_bulletin_volume_37/catch_and_effort/combined.csv",
+        show_col_types = FALSE,
+        col_types = cols(
+          .default  = col_guess(),      # keep guessing for everything else
+          asd_code  = col_character(),  # force these two
+          pot_count = col_double()
+        )
+      ) %>%
+  filter(!if_all(everything(), is.na)) %>%
+  mutate(area_code = case_when(
+    asd_code %in% c("48", "481", "482", "483", "484", "485", "486") ~ "48",
+    asd_code %in% c("58", "584", "5841", "5842", "5843", "5843a", "5843b", "5844", "5844a", "5844b", "585", "5851", "5852", "586", "587") ~ "58",
+    asd_code %in% c("88", "881", "882", "883") ~ "88",
+    TRUE ~ NA_character_
+  ))
+
+if (!dir.exists("ccamlr_data/processed_data")) {
+  dir.create("ccamlr_data/processed_data", recursive = TRUE, showWarnings = FALSE)
+}
+
+if (!dir.exists("output")) {
+dir.create("output")
+}
+
+write_csv(ds, "ccamlr_data/processed_data/ccamlr_processed_data.csv")
+
+
+
+# Build the output files
+
+
+rmarkdown::render(
+  here::here("R/create_fisheries_html.Rmd"),
+  output_file = here::here("output", "CCAMLR SoE Fisheries.html")
+)
+
+
+
+rmarkdown::render(
+  here::here("R/create_fisheries_word.Rmd"),
+  output_file = here::here("output", "CCAMLR SoE Fisheries.docx")
+)
